2b075de14b20ddd41a9db76dbd3ba0fb8abead4a
James Holmes <james.holmes@overwolf.com>
2024-05-31 16:58:47 +0100

    Add lightbox show/hide tests

--- tests/lightbox.test.ts
+++ tests/lightbox.test.ts
@@ -7,6 +7,7 @@ describe("Lightbox", () => {
     let lightbox: Lightbox;
 
     beforeEach(() => {
+        document.body.innerHTML = "";
         lightbox = new Lightbox();
     });
 
@@ -24,6 +25,40 @@ describe("Lightbox", () => {
         expect(lightbox.holder.outerHTML).toEqual(`<div class="tebex-js-lightbox__holder" role="dialog"></div>`);
     });
 
-    // TODO: test open and close -- might need to test inside a playwright instance??
+    describe("show()", () => {
+
+        test("Appends lightbox element to <body>", async () => {
+            expect(document.body.querySelector('.tebex-js-lightbox')).toBeNull();
+            await lightbox.show();
+            expect(document.body.querySelector('.tebex-js-lightbox')).not.toBeNull();
+        });
+
+        test("Only resolves once transition has finished", async() => {
+            const start = performance.now();
+            await lightbox.show();
+            const end = performance.now();
+            expect(end - start).toBeGreaterThan(400); // default duration is 410ms
+        });
+
+    });
+
+    describe("hide()", () => {
+
+        test("Removes lightbox element from <body>", async () => {
+            await lightbox.show();
+            expect(document.body.querySelector('.tebex-js-lightbox')).not.toBeNull();
+            await lightbox.hide();
+            expect(document.body.querySelector('.tebex-js-lightbox')).toBeNull();
+        });
+
+        test("Only resolves once transition has finished", async() => {
+            await lightbox.show();
+            const start = performance.now();
+            await lightbox.hide();
+            const end = performance.now();
+            expect(end - start).toBeGreaterThan(400); // default duration is 410ms
+        });
+
+    });
 
 });
