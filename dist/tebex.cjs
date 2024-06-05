3f04cb3c9c804669212c41845e560449400294b1
James Holmes <james.holmes@overwolf.com>
2024-06-05 11:01:01 +0100

    Bump version

--- dist/tebex.cjs
+++ dist/tebex.cjs
@@ -12644,7 +12644,7 @@
             isEmbedded: !popup,
             referrer: url.hostname,
             path: url.pathname,
-            version: "1.0.0"
+            version: "1.1.0"
         });
         await this.zoid.renderTo(window, container, popup ? "popup" : "iframe");
         __classPrivateFieldSet(this, _Checkout_didRender, true, "f");
@@ -12840,7 +12840,7 @@
     /**
      * Current Tebex.js package version
      */
-    const version = "1.0.0";
+    const version = "1.1.0";
     /**
      * Tebex checkout API
      */
--- dist/tebex.mjs
+++ dist/tebex.mjs
@@ -12638,7 +12638,7 @@ _Checkout_didRender = new WeakMap(), _Checkout_onRender = new WeakMap(), _Checko
         isEmbedded: !popup,
         referrer: url.hostname,
         path: url.pathname,
-        version: "1.0.0"
+        version: "1.1.0"
     });
     await this.zoid.renderTo(window, container, popup ? "popup" : "iframe");
     __classPrivateFieldSet(this, _Checkout_didRender, true, "f");
@@ -12834,7 +12834,7 @@ if (isEnvBrowser())
 /**
  * Current Tebex.js package version
  */
-const version = "1.0.0";
+const version = "1.1.0";
 /**
  * Tebex checkout API
  */
