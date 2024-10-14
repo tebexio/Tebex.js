4ab9388b3308abccead5d6cb8bfbf62823dfc7fe
Lewis Wright <lewis.wright@overwolf.com>
2024-10-14 09:47:01 +0100

    WIP

--- functions/token.js
+++ functions/token.js
@@ -1,48 +1,73 @@
+function respondWithError(message, status = 500) {
+    return new Response(JSON.stringify({ error: message }), {
+        status,
+        headers: {
+            "content-type": "application/json;charset=UTF-8",
+        },
+    });
+}
+
 export async function onRequest(request) {
     const env = request.env;
 
     let basketIdent;
 
     try {
-        let createBasketRequest = await fetch(
+        const response = await fetch(
             `${env.HEADLESS_API_ENDPOINT}/api/accounts/${env.ACCOUNT_ID}/baskets`,
             {
                 method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
                 body: JSON.stringify({
-                    username: "Notch",
+                    username: `${env.USERNAME}`,
                     complete_url: "https://tebex-js.pages.dev/complete",
                     cancel_url: "https://tebex-js.pages.dev/cancel",
                 }),
-                headers: {
-                    "content-type": "application/json;charset=UTF-8",
-                },
             }
         );
 
-        const createBasketResponse = await createBasketRequest.json();
-        basketIdent = createBasketResponse.data.ident;
+        if (!response.ok) {
+            throw new Error("Failed to create basket");
+        }
 
+        const { data } = await response.json();
+
+        console.log("Basket created", data);
+
+        basketIdent = data.ident;
     } catch (e) {
-        return new Response("Failed to create basket", {
-            status: 500,
-        });
+        return respondWithError("Failed to create basket");
     }
+
+    console.log("Basket ident: ", basketIdent);
 
     let packageIds = env.PACKAGE_IDS.split(",");
 
     for (let packageId of packageIds) {
-        await fetch(
-            `${env.HEADLESS_API_ENDPOINT}/api/baskets/${basketIdent}/packages`,
-            {
-                method: "POST",
-                body: JSON.stringify({
-                    package_id: packageId,
-                }),
-                headers: {
-                    "content-type": "application/json;charset=UTF-8",
-                },
+        try {
+            const response = await fetch(
+                `${env.HEADLESS_API_ENDPOINT}/api/baskets/${basketIdent}/packages`,
+                {
+                    method: "POST",
+                    body: JSON.stringify({
+                        package_id: packageId,
+                    }),
+                    headers: {
+                        "content-type": "application/json;charset=UTF-8",
+                    },
+                }
+            );
+
+            if (!response.ok) {
+                console.log(await response.text());
+                throw new Error("Failed to add package to basket");
             }
-        );
+        } catch (e) {
+            return respondWithError("Failed to add package to basket");
+        }
     }
 
     const json = JSON.stringify({ ident: basketIdent }, null, 2);
