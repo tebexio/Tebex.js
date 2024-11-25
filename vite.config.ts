741235098f54fbb9b8685d42725b47e83a0b0331
James Holmes <james.holmes@overwolf.com>
2024-11-25 11:36:29 +0000

    update vitest, make ident fetch use env vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import dotenv from 'dotenv';
 
 import pkg from './package.json' assert { type: 'json' };
 import { onRequest } from './functions/token';
 
+dotenv.config({ path: '.dev.vars' });
+
 const ident = await (async () => {
     console.log('Getting test basket ident...');
-    const resp = await onRequest();
+    const resp = await onRequest({ env: process.env });
     const data = await resp.json();
     return data.ident;
 })();
